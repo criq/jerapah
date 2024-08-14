acc0d0e3cc71a85719ce4f1ce2d73a29e76b492e
Jan Jablunka <jablunka@gmail.com>
2024-08-14 21:01:18 +0200

    incremental

--- src/Jerapah.php
+++ src/Jerapah.php
@@ -18,7 +18,7 @@ class Jerapah
 
 	public function __toString(): string
 	{
-		return $this->getImageUrl(400);
+		return (string)$this->getQRCode();
 	}
 
 	public static function make(string $version = null): Jerapah
@@ -78,29 +78,8 @@ class Jerapah
 		return $string;
 	}
 
-	public function getImageUrl(int $size): ?string
+	public function getQRCode(?int $size = null, ?int $margin = null): \Katu\Tools\Images\QRCode
 	{
-		try {
-			return "https://api.qrserver.com/v1/create-qr-code/?" . http_build_query([
-				"size" => "{$size}x{$size}",
-				"data" => $this->getString(),
-			]);
-		} catch (\Throwable $e) {
-			return null;
-		}
-	}
-
-	public static function generateIBAN(?string $countryCode, string $bankCode, ?string $accountPrefix, string $accountNumber): string
-	{
-		$iban = new \PHP_IBAN\IBAN(implode([
-			$countryCode ?: "CZ",
-			"00",
-			$bankCode,
-			str_pad($accountPrefix, 6, 0, \STR_PAD_LEFT),
-			str_pad($accountNumber, 10, 0, \STR_PAD_LEFT),
-		]));
-		$iban->setChecksum();
-
-		return $iban->iban;
+		return new \Katu\Tools\Images\QRCode($this->getString(), $size, $margin);
 	}
 }
